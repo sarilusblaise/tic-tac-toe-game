feda8bf71e04bba21a10f1a17076136d0053879e
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-01-25 22:30:38 -0500

    source folder have been refactored

--- src/index.js
+++ src/index.js
@@ -1,70 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-class Square extends React.Component {
-	render() {
-		return (
-			<button
-				className='square'
-				onClick={() => {
-					console.log("button have been clicked");
-				}}
-			>
-				{this.props.value}
-			</button>
-		);
-	}
-}
+import App from "./App";
 
-class Board extends React.Component {
-	renderSquare(i) {
-		return <Square value={i} />;
-	}
-
-	render() {
-		const status = "Next player: X";
-
-		return (
-			<div>
-				<div className='status'>{status}</div>
-				<div className='board-row'>
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className='board-row'>
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className='board-row'>
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
-		);
-	}
-}
-
-class Game extends React.Component {
-	render() {
-		return (
-			<div className='game'>
-				<div className='game-board'>
-					<Board />
-				</div>
-				<div className='game-info'>
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
-				</div>
-			</div>
-		);
-	}
-}
-
-// ========================================
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
