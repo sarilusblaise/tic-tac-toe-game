34ea861fc6a1a3ebbfac7976364bdd56d2f4c678
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-01-28 23:44:06 -0500

    handle click have been added to all squares

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import { useState } from "react";
 //square component represent the container for the move of each player.
-function Square({ value }) {
-	return <button className='square'>{value}</button>;
+function Square({ value, onSquareClick }) {
+	return (
+		<button className='square' onClick={onSquareClick}>
+			{value}
+		</button>
+	);
 }
 
 /* component keep all the square and allow them to communicate each other in to determine 
 when there is winner : this concept call lifting state in react : that means the state 
 of one or more child is store in a the parent component.*/
 export default function Board() {
-	const [squares, setSquare] = useState(Array(9).fill(null));
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	function handleClick(i) {
+		let nextSquares = squares.slice();
+		nextSquares[i] = "X";
+		setSquares(nextSquares);
+		console.log(nextSquares);
+	}
+
 	return (
 		<>
 			<div className='board-row'>
-				<Square value={squares[0]} />
-				<Square value={squares[1]} />
-				<Square value={squares[2]} />
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)} />
 			</div>
 			<div className='board-row'>
-				<Square value={squares[3]} />
-				<Square value={squares[4]} />
-				<Square value={squares[5]} />
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)} />
 			</div>
 			<div className='board-row'>
-				<Square value={squares[6]} />
-				<Square value={squares[7]} />
-				<Square value={squares[8]} />
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
 			</div>
 		</>
 	);
