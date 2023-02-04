d35df188b09ae3545d03063de711ac51b5af98a6
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-02-04 18:38:17 -0500

    small refactror and final cleanup

--- src/App.js
+++ src/App.js
@@ -78,20 +78,18 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const [currentMove, setCurrentMove] = useState(0);
 	const currentSquares = history[currentMove];
+	const xIsNext = currentMove % 2 === 0; // square with even number have to be always true
 	function handlePlay(nextSquares) {
 		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
 		setHistory(nextHistory);
 		setCurrentMove(nextHistory.length - 1);
-		setXIsNext(!xIsNext);
 	}
 
 	function jumpTo(nextMove) {
 		setCurrentMove(nextMove);
-		setXIsNext(currentMove % 2 === 0); // square with even number have to be always true
 	}
 
 	const moves = history.map((squares, move) => {
